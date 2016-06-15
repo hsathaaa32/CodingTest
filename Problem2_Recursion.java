6487aa407391688e864fb857f39847fe7b69302e
hsathaaa32 <satish.hsk@gmail.com>
2016-06-15 15:18:29 -0700

    Update Problem2_Recursion.java

--- Problem2_Recursion.java
+++ Problem2_Recursion.java
@@ -2,7 +2,6 @@
 public class Problem2_Recursion {
 	public String reverseString(String inputStr) {
 		//Requirements: reverse the input string by using recursion.
-		
-		return "";
+		return inputStr.charAt(inputStr.length()-1) + reverseString(inputStr.subString(0,inputStr.length()-2));
 	}
 }
