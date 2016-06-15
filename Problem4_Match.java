5cc0d90245261f338daaaa3f45752a588a2d6019
hsathaaa32 <satish.hsk@gmail.com>
2016-06-15 15:48:49 -0700

    Update Problem4_Match.java

--- Problem4_Match.java
+++ Problem4_Match.java
@@ -3,12 +3,18 @@ import java.util.Set;
 
 public class Problem4_Match {
 	public boolean doTheyMatch(Person p1, Person p2) {
-		//Are they equal?
+		if(!p1.equals(p2))
+		  return false;
 		
 		//If I add both to a Set does it enforce uniqueness?
 		Set<Person> setPerson = new HashSet<Person>();
+		setPerson.add(p1);
+		setPerson.add(p2);
+		
+		if(setPerson.size()!=1)
+		  return false;
 		
 		//If both conditions above are true, then return true, else false;
-		return false;
+		return true;
 	}
 }
