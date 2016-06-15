d28495738b5f8099b5bf80a6b28f5e3842c9df14
hsathaaa32 <satish.hsk@gmail.com>
2016-06-15 15:45:28 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -18,4 +18,19 @@ public class Person {
 	String city;
 	String state;
 	short zip;
+
+        public boolean equals(Object obj) {
+        	if(obj==this)
+        	   return true;
+        	if(!(obj instanceof Person)) {
+        		return false;
+        	}
+        	Person p = (Person) obj;
+        	return ( ((name!=null) && name.equals(p.name)) || (name==null&&p.name==null) )
+        		&& age = p.age
+        		&& ( ((address!=null) && address.equals(p.address)) || (address==null&&p.address==null) )
+        		&& ( ((city!=null) && city.equals(p.city)) || (city==null&&p.city==null) )
+        		&& ( ((state!=null) && state.equals(p.state)) || (state==null&&p.state==null) )
+        		&& zip = p.zip;
+        }
 }
