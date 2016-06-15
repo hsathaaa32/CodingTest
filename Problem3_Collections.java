42283f9a5c72dd966411eb1524dbedd96713d6ba
hsathaaa32 <satish.hsk@gmail.com>
2016-06-15 15:35:18 -0700

    Update Problem3_Collections.java

--- Problem3_Collections.java
+++ Problem3_Collections.java
@@ -6,8 +6,21 @@ public class Problem3_Collections {
 		//  Then move the numbers into a List.  Then sort the numbers.
 		//	Finally output the numbers, with one number on every row
 		
+		Map map = new HashMap();
+		Random rand = new Random();
+		for(int i=0; i<100; i++)
+		  map.put(rand.nextInt(999)+1,0);
+		Set set = new HashSet();
+		set.add(map.keySet());
 		
+		List list = new ArrayList();
+		list.add(Collections.getList(set));
 		
+		list = Arrays.asList(Arrays.sort(list.toArray()));
 		
+		for(int i=0; i<list.size(); i++) {
+			System.out.prinln(list.get(i));
+		}
+
 	}
 }
