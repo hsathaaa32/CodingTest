a1424c245a010d6f4ed6814c0d67a44197b2b28d
hsathaaa32 <satish.hsk@gmail.com>
2016-06-15 15:53:24 -0700

    Update Problem1_FizzBuzz.java

--- Problem1_FizzBuzz.java
+++ Problem1_FizzBuzz.java
@@ -5,6 +5,16 @@ public class Problem1_FizzBuzz {
 		//	If the number is a multiple of 3 output an F instead of the number.
 		//	If the number is a multiple of 5 output an B instead of the number.
 		//	If the number is a multiple of 15 output output an FB instead of the number.
+		for(int i=0; i<=100; i++) {
+			if(i%15==0)
+				System.out.println("F");
+			else if(i%3==0)
+				System.out.println("F");
+			else if(i%5==0)
+				System.out.println("B");
+			else 
+				System.out.println(i);
+		}
 		
 	}
 }
